9e022ecfc01d98515f3d24abf3fda8bc95bc3fa5
tahlia5119 <tahlia.cutifani@gmail.com>
2018-04-20 10:12:29 +0100

    Part 1 and Part 2 complete

---
+++ build/comp313p/stdr_simulator/stdr_launchers/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tahlia/cw2_test/src/comp313p/stdr_simulator/stdr_launchers
+# Build directory: /home/tahlia/cw2_test/build/comp313p/stdr_simulator/stdr_launchers
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/comp313p/stdr_simulator/stdr_resources/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tahlia/cw2_test/src/comp313p/stdr_simulator/stdr_resources
+# Build directory: /home/tahlia/cw2_test/build/comp313p/stdr_simulator/stdr_resources
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
